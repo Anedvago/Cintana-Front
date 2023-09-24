5f3793458e2c52e08cd89d983f12f57fdf5192a0
Anedvago <61481830+Anedvago@users.noreply.github.com>
2023-09-23 21:49:05 -0500

    tablas

--- src/app/modules/admin/pages/dashboard/dashboard.component.ts
+++ src/app/modules/admin/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CardRoomsComponent } from '../../components/card-rooms/card-rooms.component';
-import { CardComponent } from 'src/app/core/card/card.component';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { CardRoomsComponent } from "../../components/card-rooms/card-rooms.component";
+import { CardComponent } from "src/app/core/card/card.component";
+import { TableComponent } from "src/app/core/table/table.component";
 
 @Component({
-  selector: 'app-dashboard',
+  selector: "app-dashboard",
   standalone: true,
-  imports: [CommonModule, CardRoomsComponent, CardComponent],
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css'],
+  imports: [CommonModule, CardRoomsComponent, CardComponent, TableComponent],
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.css"],
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  columnsInd: string[] = ["Nombre Cliente", "Estado", "Habitacion"];
+  rowsInd: string[][] = [
+    ["Carlos Gomez", "Ocupado", "101"],
+    ["Pedro Corrales", "Reservado", "103"],
+    ["Oscar Rios", "Reservado", "107"],
+    ["Carlos Gomez", "Ocupado", "101"],
+    ["Pedro Corrales", "Reservado", "103"],
+    ["Oscar Rios", "Reservado", "107"],
+    ["Carlos Gomez", "Ocupado", "101"],
+    ["Pedro Corrales", "Reservado", "103"],
+    ["Oscar Rios", "Reservado", "107"],
+    ["Carlos Gomez", "Ocupado", "101"],
+    ["Pedro Corrales", "Reservado", "103"],
+    ["Oscar Rios", "Reservado", "107"],
+    ["Carlos Gomez", "Ocupado", "101"],
+    ["Pedro Corrales", "Reservado", "103"],
+    ["Oscar Rios", "Reservado", "107"],
+    ["Carlos Gomez", "Ocupado", "101"],
+    ["Pedro Corrales", "Reservado", "103"],
+    ["Oscar Rios", "Reservado", "107"],
+  ];
+  columsStock: string[] = ["Articulo", "Estado", "Stock"];
+  rowsStock: string[][] = [
+    ["Toallas", "por agotar", "10"],
+    ["Almohadas", "por agotar", "5"],
+    ["Shampoo", "por agotar", "3"],
+    ["Toallas", "agotado", "0"],
+    ["Almohadas", "por agotar", "2"],
+    ["Sábanas", "agotado", "0"],
+  ];
+}
